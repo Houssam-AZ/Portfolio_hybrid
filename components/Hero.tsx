108be0312aa0d6edab341b97b8c97e7f1a68b1fd
Houssam Aberzak <houssam.aberzak.p@gmail.com>
2024-06-14 20:14:14 +0200

    Hero TextEffect + Nav problem

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,23 +1,38 @@
 import React from "react";
-import Particle from "@/components/Particle";
+import Particle from "./Particle";
 import Image from "next/image";
+import TextEffect from "@/components/TextEffect";
+import { ArrowDownTrayIcon } from "@heroicons/react/16/solid";
 
 const Hero = () => {
     return (
-        <div className="relative h-screen overflow-hidden">
+        <div className="h-[88vh] relative bg-[url('/images/banner.jpg')] bg-cover bg-center">
             <Particle />
-            <div className="absolute inset-0 flex items-center justify-center">
-                <div className="w-[80%] mx-auto grid gap-[3rem] h-full items-center">
-                    <div className="flex flex-col lg:flex-row items-center lg:items-start">
-                        <div className="w-[500px] bg-[#55e6a5] relative flex items-center rounded-full h-[500px] order-1 lg:order-2 lg:ml-8 mb-8 lg:mb-0">
-                            <Image src="/public/images/1.jpg" alt="user" layout="fill" className="object-cover rounded-full"/>
-                        </div>
-                        <div className="flex-1 order-2 lg:order-1">
-                            <h1 className="text-[35px] md:text-[50px] text-white font-bold shadow-gray-800">
-                                HI, Im <span className="text-yellow-400">HOUSSAM!</span><br/>
-                            </h1>
-                        </div>
+            <div className="w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-full items-center relative z-10">
+                <div>
+                    <h1 className="text-[35px] md:text-[50px] text-white font-bold">
+                        HI, I'M <span className="text-yellow-400">HOUSSAM!</span>
+                        <br />
+                    </h1>
+                    <TextEffect />
+                    <p className="mt-[1.5rem] text-[18px] text-[#ffffff92]">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                        culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                    <div className="mt-[2rem] flex-col space-y-6 sm:space-y-0 sm:flex sm:flex-row items-center sm:space-x-6">
+                        <button className="px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-2">
+                            <p>Download CV</p>
+                            <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black" />
+                        </button>
                     </div>
+                </div>
+                <div className="w-[500px] hidden bg-[#55e6a5] relative lg:flex items-center rounded-full h-[500px]">
+                    <Image src="/images/1.jpg" alt="user" layout="fill" className="object-cover rounded-full" />
                 </div>
             </div>
         </div>
