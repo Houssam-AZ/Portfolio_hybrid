895905cd181646e2acf6b98b9ea73e7f52575194
Houssam Aberzak <houssam.aberzak.p@gmail.com>
2024-06-14 20:15:46 +0200

    Hero TextEffect + Nav problem

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,30 +1,30 @@
 import { Bars3Icon } from '@heroicons/react/16/solid';
-import React from 'react'
+import React from 'react';
 
 interface Props {
     openNav: () => void;
 }
 
-const Nav = ({openNav}:Props) => {
+const Nav = ({ openNav }: Props) => {
     return (
-        <div className="w-[100%] top-0 h-[12vh] bg-[#141c27] shadow-md">
-            <div className="flex items-center justify-between w-[80%] mx-auto h-[100%]">
-                <h1 className="flex-[0.6] cursor-pointer text-[25px] text-white font-bold">
-                    HOUS
-                    <span className='text-yellow-300'>DEV</span>
-                </h1>
-                <div className='nav-link'>HOME</div>
-                <div className='nav-link'>SERVICES</div>
-                <div className='nav-link'>ABOUT</div>
-                <div className='nav-link'>PROJECT</div>
-                <div className='nav-link'>BLOG</div>
-                <div className='nav-link'>CONTACT</div>
+        <div className="w-[100%] relative z-50 h-16 flex items-center justify-between px-4">
+            <h1 className="text-white text-lg font-bold">
+                HOUS
+                <span className='text-yellow-300'>DEV</span>
+            </h1>
+            <div className="flex space-x-4">
+                <div className='nav-link text-white'>HOME</div>
+                <div className='nav-link text-white'>SERVICES</div>
+                <div className='nav-link text-white'>ABOUT</div>
+                <div className='nav-link text-white'>PROJECT</div>
+                <div className='nav-link text-white'>BLOG</div>
+                <div className='nav-link text-white'>CONTACT</div>
                 <div onClick={openNav}>
-                    <Bars3Icon className="w-[2rem] md:hidden h-[2rem] cursor-pointer text-yellow-300" />
+                    <Bars3Icon className="w-6 h-6 md:hidden cursor-pointer text-yellow-300" />
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Nav;
