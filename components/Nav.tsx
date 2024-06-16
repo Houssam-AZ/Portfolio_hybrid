0e421d53ae9ffd740b327fa250c463c7354e6b96
Houssam Aberzak <houssam.aberzak.p@gmail.com>
2024-06-16 18:16:16 +0200

    NavBar Adjustments

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,4 +1,5 @@
 import { Bars3Icon } from '@heroicons/react/16/solid';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
 import React from 'react';
 
 interface Props {
@@ -12,15 +13,18 @@ const Nav = ({ openNav }: Props) => {
                 HOUS
                 <span className='text-yellow-300'>DEV</span>
             </h1>
-            <div className="flex space-x-4">
-                <div className='nav-link text-white'>HOME</div>
-                <div className='nav-link text-white'>SERVICES</div>
+            <div className="flex space-x-4 items-center">
                 <div className='nav-link text-white'>ABOUT</div>
-                <div className='nav-link text-white'>PROJECT</div>
-                <div className='nav-link text-white'>BLOG</div>
-                <div className='nav-link text-white'>CONTACT</div>
-                <div onClick={openNav}>
-                    <Bars3Icon className="w-6 h-6 md:hidden cursor-pointer text-yellow-300" />
+                <div className='nav-link text-white'>SKILLS</div>
+                <div className='nav-link text-white'>PROJECTS</div>
+                <a href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin className="w-6 h-6 text-white hover:text-yellow-300" />
+                </a>
+                <a href="https://github.com/your-github-profile" target="_blank" rel="noopener noreferrer">
+                    <FaGithub className="w-6 h-6 text-white hover:text-yellow-300" />
+                </a>
+                <div onClick={openNav} className="md:hidden cursor-pointer">
+                    <Bars3Icon className="w-6 h-6 text-yellow-300" />
                 </div>
             </div>
         </div>
